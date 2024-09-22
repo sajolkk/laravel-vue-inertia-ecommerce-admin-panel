4dfd0cd1d4207acae59a30f713c1fdb6315265d5
sajolkk <sajolkar56@gmail.com>
2024-09-23 01:29:52 +0600

    sidebar menu dropdown has been updated

--- resources/js/Components/Menu/DropDownMenu.vue
+++ resources/js/Components/Menu/DropDownMenu.vue
@@ -1,11 +1,12 @@
 <script setup>
     defineProps({
-        menu: Object()
+        menu: Object,
+        toggleDropDownMenu: Function,
     });
 </script>
 <template>
     <li class="group text-gray-300 hover:text-white" :class="menu.isOpen ? 'text-white' : null">
-        <div class="flex justify-between items-center cursor-pointer py-2 rounded-md hover:bg-[#ffffff33] px-2">
+        <div class="flex justify-between items-center cursor-pointer py-2 rounded-md hover:bg-[#ffffff33] px-2" :class="menu.isOpen ? 'bg-[#ffffff33]' : null" @click="toggleDropDownMenu(menu.name)">
             <!-- bg-[#78aaf03b] -->
             <div>
                 <span class="pr-2 w-[60px]">
@@ -22,7 +23,7 @@
             <li v-for="submenu in menu.subMenus" :key="submenu.name" class="py-2 rounded-md cursor-pointer text-gray-300 hover:text-white hover:bg-[#ffffff33] px-2">
                 <a :href="submenu.route">
                     <span class="pr-2">
-                        <FontAwesomeIcon :icon="['fas', 'home']" size="xl"/>
+                        <FontAwesomeIcon :icon="['fas', submenu.icon]" size="xl"/>
                     </span>
                     <span class="px-0">{{ submenu.name }}</span>
                 </a>
--- resources/js/Layouts/SidebarMenu.vue
+++ resources/js/Layouts/SidebarMenu.vue
@@ -1,211 +1,236 @@
 <script setup>
-    import Menu from '@/Components/Menu/Menu.vue';
-    import DropDownMenu from '@/Components/Menu/DropDownMenu.vue';
-    
-    const menus = [
-        {
-            name: 'Dashboard',
-            icon: 'home',
-            isOpen: false,
-            isSubMenu: false,
-            subMenus: [],
-            route: route('dashboard')
-        },
-        {
-            name: 'Category',
-            icon: 'columns',
-            isOpen: false,
-            isSubMenu: true,
-            route: '',
-            subMenus: [
-                {
-                    name: 'New Category',
-                    icon: 'plus-circle',
-                    route: route('dashboard')
-                },
-                {
-                    name: 'Category List',
-                    icon: 'clipboard-list',
-                    route: route('dashboard')
-                }
-            ],
-        },
-        {
-            name: 'Variation',
-            icon: 'table',
-            isOpen: false,
-            isSubMenu: false,
-            subMenus: [],
-            route: '',
-        },
-        {
-            name: 'Brand',
-            icon: 'bold',
-            isOpen: false,
-            isSubMenu: true,
-            route: '',
-            subMenus: [
-                {
-                    name: 'New Brand',
-                    icon: 'plus-circle',
-                    route: route('dashboard')
-                },
-                {
-                    name: 'Brand List',
-                    icon: 'clipboard-list',
-                    route: route('dashboard')
-                }
-            ],
-        },
-        {
-            name: 'Product',
-            icon: 'box',
-            isOpen: false,
-            isSubMenu: true,
-            route: '',
-            subMenus: [
-                {
-                    name: 'New Product',
-                    icon: 'plus-circle',
-                    route: route('dashboard')
-                },
-                {
-                    name: 'Product List',
-                    icon: 'clipboard-list',
-                    route: route('dashboard')
-                }
-            ],
-        },
-        {
-            name: 'Supplier',
-            icon: 'truck',
-            isOpen: false,
-            isSubMenu: true,
-            route: '',
-            subMenus: [
-                {
-                    name: 'New Supplier',
-                    icon: 'plus-circle',
-                    route: route('dashboard')
-                },
-                {
-                    name: 'Supplier List',
-                    icon: 'clipboard-list',
-                    route: route('dashboard')
-                }
-            ],
-        },
-        {
-            name: 'Purchase',
-            icon: 'shopping-bag',
-            isOpen: false,
-            isSubMenu: true,
-            route: '',
-            subMenus: [
-                {
-                    name: 'New Purchase',
-                    icon: 'plus-circle',
-                    route: route('dashboard')
-                },
-                {
-                    name: 'Purchase List',
-                    icon: 'clipboard-list',
-                    route: route('dashboard')
-                }
-            ],
-        },
-        {
-            name: 'Offer',
-            icon: 'gift',
-            isOpen: false,
-            isSubMenu: true,
-            route: '',
-            subMenus: [
-                {
-                    name: 'New Offer',
-                    icon: 'plus-circle',
-                    route: route('dashboard')
-                },
-                {
-                    name: 'Offer List',
-                    icon: 'clipboard-list',
-                    route: route('dashboard')
-                }
-            ],
-        },
-        {
-            name: 'Order',
-            icon: 'shopping-cart',
-            isOpen: false,
-            isSubMenu: true,
-            route: '',
-            subMenus: [
-                {
-                    name: 'Order Report',
-                    icon: 'circle',
-                    route: route('dashboard')
-                }
-            ],
-        },
-        {
-            name: 'PC Builder Component',
-            icon: 'laptop',
-            isOpen: false,
-            isSubMenu: true,
-            route: '',
-            subMenus: [
-                {
-                    name: 'New Component',
-                    icon: 'plus-circle',
-                    route: route('dashboard')
-                },
-                {
-                    name: 'Component List',
-                    icon: 'clipboard-list',
-                    route: route('dashboard')
-                }
-            ],
-        },
-        {
-            name: 'Setting',
-            icon: 'wrench',
-            isOpen: false,
-            isSubMenu: true,
-            route: '',
-            subMenus: [
-                {
-                    name: 'Website Setting',
-                    icon: 'cog',
-                    route: route('dashboard')
-                },
-                {
-                    name: 'Shipping Area',
-                    icon: 'location-arrow',
-                    route: route('dashboard')
-                }
-            ],
-        },
-    ];
+import Menu from "@/Components/Menu/Menu.vue";
+import DropDownMenu from "@/Components/Menu/DropDownMenu.vue";
+import { ref } from "vue";
+
+const menus = ref([
+    {
+        name: "Dashboard",
+        icon: "home",
+        isOpen: false,
+        isSubMenu: false,
+        subMenus: [],
+        route: route("dashboard"),
+    },
+    {
+        name: "Category",
+        icon: "columns",
+        isOpen: false,
+        isSubMenu: true,
+        route: "",
+        subMenus: [
+            {
+                name: "New Category",
+                icon: "plus-circle",
+                route: route("dashboard"),
+            },
+            {
+                name: "Category List",
+                icon: "clipboard-list",
+                route: route("dashboard"),
+            },
+        ],
+    },
+    {
+        name: "Variation",
+        icon: "table",
+        isOpen: false,
+        isSubMenu: false,
+        subMenus: [],
+        route: "",
+    },
+    {
+        name: "Brand",
+        icon: "bold",
+        isOpen: false,
+        isSubMenu: true,
+        route: "",
+        subMenus: [
+            {
+                name: "New Brand",
+                icon: "plus-circle",
+                route: route("dashboard"),
+            },
+            {
+                name: "Brand List",
+                icon: "clipboard-list",
+                route: route("dashboard"),
+            },
+        ],
+    },
+    {
+        name: "Product",
+        icon: "box",
+        isOpen: false,
+        isSubMenu: true,
+        route: "",
+        subMenus: [
+            {
+                name: "New Product",
+                icon: "plus-circle",
+                route: route("dashboard"),
+            },
+            {
+                name: "Product List",
+                icon: "clipboard-list",
+                route: route("dashboard"),
+            },
+        ],
+    },
+    {
+        name: "Supplier",
+        icon: "truck",
+        isOpen: false,
+        isSubMenu: true,
+        route: "",
+        subMenus: [
+            {
+                name: "New Supplier",
+                icon: "plus-circle",
+                route: route("dashboard"),
+            },
+            {
+                name: "Supplier List",
+                icon: "clipboard-list",
+                route: route("dashboard"),
+            },
+        ],
+    },
+    {
+        name: "Purchase",
+        icon: "shopping-bag",
+        isOpen: false,
+        isSubMenu: true,
+        route: "",
+        subMenus: [
+            {
+                name: "New Purchase",
+                icon: "plus-circle",
+                route: route("dashboard"),
+            },
+            {
+                name: "Purchase List",
+                icon: "clipboard-list",
+                route: route("dashboard"),
+            },
+        ],
+    },
+    {
+        name: "Offer",
+        icon: "gift",
+        isOpen: false,
+        isSubMenu: true,
+        route: "",
+        subMenus: [
+            {
+                name: "New Offer",
+                icon: "plus-circle",
+                route: route("dashboard"),
+            },
+            {
+                name: "Offer List",
+                icon: "clipboard-list",
+                route: route("dashboard"),
+            },
+        ],
+    },
+    {
+        name: "Order",
+        icon: "shopping-cart",
+        isOpen: false,
+        isSubMenu: true,
+        route: "",
+        subMenus: [
+            {
+                name: "Order Report",
+                icon: "circle",
+                route: route("dashboard"),
+            },
+        ],
+    },
+    {
+        name: "PC Builder Component",
+        icon: "laptop",
+        isOpen: false,
+        isSubMenu: true,
+        route: "",
+        subMenus: [
+            {
+                name: "New Component",
+                icon: "plus-circle",
+                route: route("dashboard"),
+            },
+            {
+                name: "Component List",
+                icon: "clipboard-list",
+                route: route("dashboard"),
+            },
+        ],
+    },
+    {
+        name: "Setting",
+        icon: "wrench",
+        isOpen: false,
+        isSubMenu: true,
+        route: "",
+        subMenus: [
+            {
+                name: "Website Setting",
+                icon: "cog",
+                route: route("dashboard"),
+            },
+            {
+                name: "Shipping Area",
+                icon: "location-arrow",
+                route: route("dashboard"),
+            },
+        ],
+    },
+]);
+
+const toggleDropDownMenu = (currentMenu) => {
+    menus.value.forEach((menu) => {
+        if (menu.name === currentMenu && menu.isOpen === true) {
+            menu.isOpen = false;
+        } else {
+            menu.isOpen = menu.name === currentMenu;
+        }
+    });
+};
+
+
 </script>
 <template>
-    <div class="hidden md:block w-[300px] shadow-2xl h-screen dark:bg-gray-800 py-2 fixed top-0 left-0">
+    <div
+        class="hidden md:block w-[260px] shadow-2xl h-screen dark:bg-gray-800 py-2 fixed top-0 left-0"
+    >
         <!-- Fixed Top Logo Section -->
         <div class="sticky top-0 bg-gray-800 z-10">
-            <a href="" class="flex justify-center items-center cursor-pointer text-gray-300 hover:text-white pb-[10px] border-b-[1px] border-gray-600">
+            <a
+                href=""
+                class="flex justify-center items-center cursor-pointer text-gray-300 hover:text-white pb-[10px] border-b-[1px] border-gray-600"
+            >
                 <div>
-                <img src="https://admin.sopmatech.com/assets/uploads/setting/logo/1705161862.jpeg" width="70" alt="">
+                    <img
+                        src="https://admin.sopmatech.com/assets/uploads/setting/logo/1705161862.jpeg"
+                        width="70"
+                        alt=""
+                    />
                 </div>
-                <div class="pl-3 font-bold">
-                Sopma Tech
-                </div>
+                <div class="pl-3 font-bold">Sopma Tech</div>
             </a>
         </div>
         <!-- Scrollable Menu Section -->
-        <nav class="pt-1 mt-2 h-[calc(100vh-80px)] overflow-y-auto scrollbar px-2">
+        <nav
+            class="pt-1 mt-2 h-[calc(100vh-80px)] overflow-y-auto scrollbar px-2"
+        >
             <ul class="list-none space-y-1">
                 <template v-for="menu in menus" :key="menu.name">
                     <template v-if="menu.isSubMenu">
-                        <DropDownMenu :menu="menu" />
+                        <DropDownMenu
+                            :menu="menu"
+                            :toggleDropDownMenu="toggleDropDownMenu"
+                        />
                     </template>
 
                     <template v-else>
